96ae536966b2250ea530248cf3a076af4a2c6c40
Hanwen <heawen.cheng@gmail.com>
2014-05-09 21:38:59 +0200

    add auth setup

--- server.js
+++ server.js
@@ -2,7 +2,36 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
-app.use(logfmt.requestLogger());
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var logger = require('morgan');
+
+var configDB = require('./config/database.js');
+
+//--------------------configuration------------------------
+mongoose.connect(configDB.url); // connect to our database
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+
+if ('development' === app.get('env')) {
+// set up our express application
+    app.use(logfmt.requestLogger()); //another logger other than morgan
+    app.use(logger('dev')); // set the logger
+    app.use(express.cookieParser()); // read cookies (needed for auth)
+    app.use(express.bodyParser()); // get information from html forms
+
+    app.set('view engine', 'ejs'); // set up ejs for templating TODO
+
+// required for passport
+    app.use(express.session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+    app.use(passport.initialize());
+    app.use(passport.session()); // persistent login sessions
+    app.use(flash()); // use connect-flash for flash messages stored in session
+
+}
+
 app.use(express.static(__dirname + '/public'));
 
  app.get('/' , function(request, response){
