6cbe20208ff22b4c6e503e7fd0385140cf221c09
Hanwen <heawen.cheng@gmail.com>
2014-05-13 22:42:29 +0200

    integrate auth and zugzug

--- app/routes.js
+++ app/routes.js
@@ -5,7 +5,7 @@ module.exports = function(app, passport) {
     // HOME PAGE (with login links) ========
     // =====================================
     app.get('/', function(req, res) {
-        res.render('index.ejs'); // load the index.ejs file
+        res.render('index.html'); // load the index.ejs file
     });
 
     // =====================================
@@ -16,7 +16,7 @@ module.exports = function(app, passport) {
         //---req.flash: This is the connect-flash way of getting flashdata in the session.
         // We will create the loginMessage inside our passport configuration
         // render the page and pass in any flash data if it exists
-        res.render('login.ejs', { message: req.flash('loginMessage') });
+        res.render('login.html', { message: req.flash('loginMessage') });
     });
 
     // process the login form
@@ -33,7 +33,7 @@ module.exports = function(app, passport) {
     app.get('/signup', function(req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('signup.ejs', { message: req.flash('signupMessage') });
+        res.render('signup.html', { message: req.flash('signupMessage') });
     });
 
 //    process the signup form
@@ -50,7 +50,7 @@ module.exports = function(app, passport) {
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
     app.get('/profile', isLoggedIn, function(req, res) {
-        res.render('profile.ejs', {
+        res.render('profile.html', {
             user : req.user // get the user out of session and pass to template
         });
     });
--- server.js
+++ server.js
@@ -9,6 +9,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var engines = require('consolidate');
+//var handlebars = require('handlebars');
 
 var configDB = require('./config/database.js');
 
@@ -25,7 +27,13 @@ if ('development' === app.get('env')) {
     app.use(cookieParser()); // read cookies (needed for auth)
     app.use(bodyParser()); // get information from html forms
 
-    app.set('view engine', 'ejs'); // set up ejs for templating TODO
+//    app.set('view engine', 'ejs'); // set up ejs for templating TODO
+    app.set('views', __dirname+'/views');
+    app.set('view engine', 'html');
+    app.engine('html', engines.handlebars, function(err, html) {
+        if (err) throw err;
+        console.log("successful load handlebars engine"); // why there is no log?TODO
+    });
 
 // required for passport
     app.use(session({ secret: 'heawen110' })); // session secret
@@ -35,7 +43,18 @@ if ('development' === app.get('env')) {
 
 }
 
-//app.use(express.static(__dirname + '/public'));
+//==========================================================================
+//==============================static file system==========================
+//==========================================================================
+
+//var http = require('http');
+//var parse = require('url').parse;
+//var join = require('path').join;
+//var fs = require('fs');
+//
+//var root = __dirname + '/public';
+
+app.use(express.static(__dirname));
 
 // app.get('/' , function(request, response){
 //       response.redirect('/index.html');
